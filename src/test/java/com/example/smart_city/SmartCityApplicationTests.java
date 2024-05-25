75eafa737a09ac7e57fc6d807b63ca4f5dd251ac
Loresa <loresag17@gmail.com>
2024-05-25 10:55:26 +0200

    first commit

---
+++ src/test/java/com/example/smart_city/SmartCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_city;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
